ec045a8609f36b7f312a3bd428458cf45b39b3f7
sw <shadow.bfs@gmail.com>
2020-11-05 13:07:41 +0300

    correct connector pass

--- twint/__init__.py
+++ twint/__init__.py
@@ -1,7 +1,7 @@
 from .config import Config
+from .errors import *
+from .get import get_user_id, search_url, profile_feed_url
+from .parser import parse_tweets
 from .search import TwintSearch
 from .token import TokenGetter
 from .user_agents import default_user_agent, get_random_user_agent
-from .get import get_user_id, search_url, profile_feed_url
-from .parser import parse_tweets
-from .errors import *
